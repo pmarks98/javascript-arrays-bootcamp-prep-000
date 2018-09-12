990bf6320a82fffaa63d9f7b5265ebac56de73ab
Pierson Marks <pmarks98@gmail.com>
2018-09-12 23:55:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,11 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-    array.shift;
+    array.shift();
+    return array;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+    array.shift();
     return array;
 }
