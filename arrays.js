234d3ab4bcb222ab8c4eb0f7a090042354c154ec
Pierson Marks <pmarks98@gmail.com>
2018-09-13 00:04:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-    return array.slice(0);
+    print array;
+    return array.slice(1);
 }
 
 
